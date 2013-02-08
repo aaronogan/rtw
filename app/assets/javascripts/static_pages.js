322f9c5ba68b48249f6b366e88448b00075db3df
Aaron Ogan <aaron.ogan@gmail.com>
2013-02-08 21:15:36 +0000

    Cleaning up map js.

--- app/assets/javascripts/static_pages.js
+++ app/assets/javascripts/static_pages.js
@@ -2,18 +2,12 @@ var rtwMap = new function () {
   var map = null;
   var mapOptions = {
     center: new google.maps.LatLng(0, 0),
-    zoom: 8,
+    zoom: 2,
     mapTypeId: google.maps.MapTypeId.ROADMAP
   };
 
   return {
     init: function () {
-     console.log('init');
-      var mapOptions = {
-        center: new google.maps.LatLng(0, 0),
-        zoom: 8,
-        mapTypeId: google.maps.MapTypeId.ROADMAP
-      };
       map = new google.maps.Map(document.getElementById('map_canvas'), mapOptions);
     }
   }
