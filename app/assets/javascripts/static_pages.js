6c0e2688698a23b3d3307944f79926edd5c8ec19
Aaron Ogan <aaron.ogan@gmail.com>
2013-02-08 22:33:12 +0000

    Plotting locations.

--- app/assets/javascripts/static_pages.js
+++ app/assets/javascripts/static_pages.js
@@ -6,9 +6,32 @@ var rtwMap = new function () {
     mapTypeId: google.maps.MapTypeId.ROADMAP
   };
 
+  var locations = [{
+    name: 'Chicago',
+    lat: 41.9,
+    lng: -87.65
+  },
+  {
+    name: 'Reykjavik, Iceland',
+    lat: 64.14,
+    lng: -21.9
+  }];
+
+  var plotLocation = function (location) {
+    var marker = new google.maps.Marker({
+      position: new google.maps.LatLng(location.lat, location.lng),
+      map: map,
+      title: location.name
+    });
+  }
+
   return {
     init: function () {
       map = new google.maps.Map(document.getElementById('map_canvas'), mapOptions);
+      $.each(locations, function () {
+        console.log(this);
+        plotLocation(this);
+      });
     }
   }
 }
