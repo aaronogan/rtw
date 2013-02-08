d716acd04dd70ab20ac71a2b7e6f3f98e03cf1f2
Aaron Ogan <aaron.ogan@gmail.com>
2013-02-08 20:45:18 +0000

    Setting up Google map canvas.

---
+++ app/assets/javascripts/static_pages.js
@@ -0,0 +1,14 @@
+function initMap() {
+  var mapOptions = {
+    center: new google.maps.LatLng(-34.397, 150.644),
+    zoom: 8,
+    mapTypeId: google.maps.MapTypeId.ROADMAP
+  };
+  
+  var map = new google.maps.Map(document.getElementById('map_canvas'), mapOptions);
+}
+
+$(document).ready(function() {
+  console.log('load');
+  initMap();
+});
