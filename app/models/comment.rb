354f907af45d35906781abf63b5ab9189af3687e
Aaron Ogan <aaron.ogan@gmail.com>
2013-02-13 16:44:07 +0000

    Setting comment/location relationship.

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,3 +1,4 @@
 class Comment < ActiveRecord::Base
-  attr_accessible :content, :name, :url
+  attr_accessible :content, :name, :url, :location_id
+  belongs_to :location
 end
--- app/models/location.rb
+++ app/models/location.rb
@@ -1,3 +1,4 @@
 class Location < ActiveRecord::Base
   attr_accessible :lat, :lon, :name, :sequence, :photoset
+  has_many :comments
 end
